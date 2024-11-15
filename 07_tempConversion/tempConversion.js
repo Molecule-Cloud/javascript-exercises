5263c47dfc382ba044b336bec8de88014632b670
Beno <66329054+Molecule-Cloud@users.noreply.github.com>
2024-11-15 17:25:20 +0000

    All tempConversion tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,23 @@
-const convertToCelsius = function() {
-};
+const roundedNumber = function(num)
+{
+	return Math.round(num * 10) / 10;
+}
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(unit) 
+{
+	let conversion = (unit - 32) * (5 / 9);
+	rounded = roundedNumber(conversion);
+	return rounded;
 };
+console.log(convertToCelsius());
 
+const convertToFahrenheit = function(unit) 
+{
+	conversion = (unit * (9 / 5) + 32)
+	rounded = roundedNumber(conversion);
+	return rounded;
+};
+console.log(convertToFahrenheit());
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
