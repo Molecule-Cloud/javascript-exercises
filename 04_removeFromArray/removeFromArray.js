4f96e2fa4561805b9ce9c6412b508534764b04e1
Beno <66329054+Molecule-Cloud@users.noreply.github.com>
2024-11-15 14:48:23 +0000

    All Tests Passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,22 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args)
+{
+	// Make a copy of the original Array
+	let new_arr = [...arr];
+	let i = 0;
+	//Loop through the length of the Array
+	for(; i < new_arr.length; i++)
+	{
+		if(args.includes(new_arr[i]))
+		{
+	//Remove element at the given index
+			new_arr.splice(i, 1);
+	// Adjust arry to account for the removed element
+			i--;
+		}
+	}
+	return new_arr;
 };
+console.log(removeFromArray([]), )
 
 // Do not edit below this line
 module.exports = removeFromArray;
